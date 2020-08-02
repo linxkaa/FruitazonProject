49a39e1247f53a1303de18c73476f72a16d3e93a
maesamaziahgit <66314241+maesamaziahgit@users.noreply.github.com>
2020-08-02 14:52:06 +0700

    test

--- fruitazonfinal/src/App.js
+++ fruitazonfinal/src/App.js
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Shop from "./components/Shop";
 import Contact from "./components/Contact";
+import About from "./components/About";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shop} />
         <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
       </div>
     );
   }
