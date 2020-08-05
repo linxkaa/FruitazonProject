dbeba27ed3339779e170ff5dd924dfd3f3d309ff
maesamaziahgit <66314241+maesamaziahgit@users.noreply.github.com>
2020-08-05 11:49:31 +0700

    create menu about

--- fruitazonfinal/public/assets/js/main.js
+++ fruitazonfinal/public/assets/js/main.js
@@ -199,6 +199,47 @@
         }
     });
 
+    /*------------------
+		Testimonial Active
+	--------------------*/
+    // review-active
+    $('.testmonial_active').owlCarousel({
+        loop:true,
+        margin:0,
+    items:1,
+    autoplay:true,
+    navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+        nav:true,
+    dots:false,
+    autoplayHoverPause: true,
+    autoplaySpeed: 800,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+        responsive:{
+            0:{
+                items:1,
+                dots:false,
+                nav:false,
+            },
+            767:{
+                items:1,
+                dots:false,
+                nav:false,
+            },
+            992:{
+                items:1,
+                nav:false
+            },
+            1200:{
+                items:1,
+                nav:false
+            },
+            1500:{
+                items:1
+            }
+        }
+    });
+    
     /*-------------------
 		Quantity change
 	--------------------- */
