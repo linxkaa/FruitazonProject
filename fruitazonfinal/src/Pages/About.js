6446152be7592e077d7f67902c16d9ea0dc5a513
Lintang Kusuma <lintangkusuma04@gmail.com>
2020-08-09 10:11:22 +0700

    hai

---
+++ fruitazonfinal/src/Pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Navbar from "./widgets/Navbar";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default About;
